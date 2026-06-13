4d86f91786588874ef8b0eb79e609a186169e175
onur123ck123-ship-it <onur123ck123@gmail.com>
2026-06-14 00:45:04 +0300

    Show place add only to owner

--- google_cafe_sync_ui.js
+++ google_cafe_sync_ui.js
@@ -1,10 +1,21 @@
 (function(){
 window.__googleCafeSyncUI=1;
+function mine(){
+ try{
+  var p=JSON.parse(localStorage.ca_profile||'{}');
+  return String(p.username||p.user||'').toLowerCase()==='onurcan';
+ }catch(e){return false}
+}
 function off(){
- ['gcsBtn','gcsBox','uapBtn'].forEach(function(i){
+ ['gcsBtn','gcsBox'].forEach(function(i){
   var x=document.getElementById(i);
   if(x)x.remove();
  });
+ var b=document.getElementById('uapBtn');
+ if(b){
+  if(mine())b.style.display='block';
+  else b.remove();
+ }
 }
 off();
 setInterval(off,1000);
