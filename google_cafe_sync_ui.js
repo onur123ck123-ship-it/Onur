ccc4d43f0ea4b93d5f37d1cca45683c918145c58
onur123ck123-ship-it <onur123ck123@gmail.com>
2026-06-14 00:44:48 +0300

    Hide old map utility buttons

--- google_cafe_sync_ui.js
+++ google_cafe_sync_ui.js
@@ -1,10 +1,10 @@
 (function(){
 window.__googleCafeSyncUI=1;
 function off(){
- var b=document.getElementById('gcsBtn');
- if(b)b.remove();
- var x=document.getElementById('gcsBox');
- if(x)x.remove();
+ ['gcsBtn','gcsBox','uapBtn'].forEach(function(i){
+  var x=document.getElementById(i);
+  if(x)x.remove();
+ });
 }
 off();
 setInterval(off,1000);
